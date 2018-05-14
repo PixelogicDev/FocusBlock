df49b27716074e600b2627583bc34e0e7a299ca6
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-05-14 05:41:20 -0500

    Adding initial react project with basic dashboard comp #10

---
+++ focus-block/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Dashboard from './Components/Dashboard/index';
+
+class App extends Component {
+  render() {
+    return <Dashboard />;
+  }
+}
+
+export default App;
---
+++ focus-block/src/Components/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return <h1> Hello From The Dashboard </h1>;
+  }
+}
+
+export default Dashboard;
