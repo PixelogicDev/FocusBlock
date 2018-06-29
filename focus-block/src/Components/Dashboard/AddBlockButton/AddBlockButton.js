2f5927f0c1e8fe7736e8ad799a9928cf5d3d8a13
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-06-29 07:05:27 -0500

    Tweaking add button to show click #14

--- focus-block/src/Components/Dashboard/AddBlockButton/AddBlockButton.js
+++ focus-block/src/Components/Dashboard/AddBlockButton/AddBlockButton.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class AddBlockButton extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			didClick: false
+		};
+	}
+
+	//-- Helpers -- //
+	addBlock = () => {
+		// Set didClick state //
+		this.setState({
+			didClick: true
+		});
+
+		// Trigger addBlock event //
+		this.props.addBlockTrigger();
+
+		// Set State back after animation //
+		setTimeout(() => {
+			this.setState({
+				didClick: false
+			});
+		}, 100);
+	};
+
 	render() {
 		return (
 			<img
-				className="action-add"
-				onClick={this.props.addBlockTrigger}
+				className={this.state.didClick ? 'action-add scale' : 'action-add'}
+				onClick={this.addBlock}
 				//-- MAD PROPS Tendencydriven --//
 				src={require('../../../Assets/cmjio-button@2x.png')}
 				alt="Add Focus Block Button"
